1aec99952cddd21cb3d98361d9913eed334dc34f
ares0uyu <ares0uyu@gmail.com>
2024-07-04 11:13:49 +0900

    update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,16 @@
 // sum.cpp
 #include "sum.h"
 
+
 int sum(int n) {
-	int res = 0;
-	for (int i = 1; i <= n; ++i)
-		res += i;
-	return res;
+	return n * (n - 1) / 2;
 }
+
+
+//int sum(int n) {
+//	int res = 0;
+//	for (int i = 1; i <= n; ++i)
+//		res += i;
+//	return res;
+//}
 
